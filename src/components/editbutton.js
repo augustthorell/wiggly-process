ad2752daa4c7ef147c969504fc359e0c636c7199
August Thorell <augustthorell@gmail.com>
2021-02-10 14:34:20 +0100

    added put method

--- src/components/editbutton.js
+++ src/components/editbutton.js
@@ -1,41 +1,69 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function EditButton(articles) {
-    console.log(articles.id)
     
+    let article = articles.id
     
 
-    function changeHandler(e) {
-        this.setState({[e.target.name]: e.target.value})
+    const [editTitle, setTitle] = useState ('');
+    const [editBody, setBody] = useState ('');
+
+    const submitHandler = e => {
+        e.preventDefault()
+        let myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        let raw = JSON.stringify(
+            {
+                "id": parseInt(article.id),
+                "title": editTitle,
+                "body": editBody
+            });
+
+        let requestOptions = {
+        method: 'PUT',
+        body: raw,
+        redirect: 'follow'
+        };
+
+        fetch("http://localhost:8081/api/articles.php/", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+
+        window.location.reload();
     }
-    
+
     return (
         <div>
-            <button type="button" /* onClick={ () => displayEdit()} */>Edit</button><br />
-            <input
-                className="titletextedit"
-                type="text"
-                name="edittitle"
-                placeholder="Edit Title"
-                value={articles.id.title}
-                onChange={changeHandler}
-                /* style={{display: "none"}} */
-            />
-            <br />
-            <textarea
-                className="bodytextedit"
-                style={{
-                    height: 280,
-                    width: 550,
-                }}
-                name="editbody"
-                placeholder="Edit Content"
-                value={articles.id.body}
-                onChange={changeHandler}
-                /* style={{display: "none"}} */
-            />
+            <form onSubmit={submitHandler}>
+                <input
+                    className="titletextedit"
+                    type="text"
+                    name="edittitle"
+                    placeholder="Edit Title"
+                    value={editTitle} 
+                    onChange={e => setTitle(e.target.value)} 
+                    /* style={{display: "none"}} */
+                />
+                <br />
+                <textarea
+                    className="bodytextedit"
+                    style={{
+                        height: 280,
+                        width: 550,
+                    }}
+                    name="editbody"
+                    placeholder="Edit Content"
+                    value={editBody}
+                    onChange={e => setBody(e.target.value)}
+                    /* style={{display: "none"}} */
+                />
+                <button type="submit" >Save edit</button><br />
+            </form>
         </div>
     )
 }
 
-export default EditButton
+export default EditButton 
+
